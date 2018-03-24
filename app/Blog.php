3dcb134945c880c4c1be86cd6c850b06fbaf96e0
faizaaulia <faizaaulia20@gmail.com>
2018-03-24 10:42:56 +0700

    eloquent CRUD

--- app/Blog.php
+++ app/Blog.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    protected $table = 'blog';
+    protected $table = 'blog'; //kalo nama tabel di db "blogs", gausah pake protected. kalo bukan blogs, harus pake protected
     public $timestamps = false; //created_at dan updated_at gaada
+
+    //insert assignment
+    // protected $fillable = ['title', 'description']; //whitelist (yang boleh diisi)
+    protected $guarded = []; //blacklist (yang gaboleh diisi)
 }
--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,6 +10,38 @@ class BlogController extends Controller
 {
     public function index()
     {
+      //insert biasa
+      // $blog = new Blog;
+      // $blog->title = 'Judul Blog Tiga';
+      // $blog->description = 'Isi dari "Judul Blog Tiga" yaaa';
+      // $blog->save();
+
+      //insert mass assignment
+      // Blog::create([
+      //   'title' => 'Judul Blog Lima',
+      //   'description' => 'Isi dari "Judul Blog Lima" yaaa'
+      // ]);
+
+      //updated
+      // $blog = Blog::where('title', 'Judul Blog Lima')->first();
+      // //kalo update dipilih dulu mana yg mau diupdate, terus ditaruh di variabel
+      // $blog->title = 'Judul Blog Enam';
+      // $blog->description = 'Isi dari "Judul Blog Enam" yaaa';
+      // $blog->save();
+
+      //update mass assignment
+      // Blog::find(6)->update([
+      //   'title' => 'Judul Blog Enam lalala',
+      //   'description' => 'Isi dari "Judul Blog Enam" yaaalalala'
+      // ]);
+
+      //delete
+      // $blog = Blog::find(6);
+      // $blog->delete();
+
+      //delete ke dua
+      Blog::destroy(5);
+
       $blog = Blog::all();
 
       return view('blog', ['blog' => $blog]);
@@ -31,8 +63,8 @@ class BlogController extends Controller
       //delete
       // DB::table('user')->where('id', '3')->delete();
 
-      $blog = Blog::find($id);
-      
+      $blog = Blog::findOrFail($id); //kalo pake find() tampilan errornya banyak, kalo findOrFail error "Not Found"
+
       return view('single', ['blog' => $blog]);
     }
 }
